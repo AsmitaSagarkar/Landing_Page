4ad94645b35fd5c493348c590b3ccb8849132602
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-12-21 00:00:14 +0530

    initial commit

---
+++ src/component/header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header(){
+    return (
+        <>
+        <h1 className="text-yellow-300">hello</h1>
+        <h2 className="text-purple-700" >heyy</h2>
+        </>
+        
+    )
+}
