a7b064f80fd21ae9e1f4585158cd46f095783909
Asmita Sagarkar <asmitasagarkar@gmail.com>
2023-12-22 02:36:34 +0530

    images added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      
+      gradient : "bg-gradient-to-top from #D8B4FE to #D8B4FE00"
+      },
+
+
+    },
   },
   plugins: [],
 };
